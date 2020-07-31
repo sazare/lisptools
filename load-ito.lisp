14b250d53c2403dbecc39a5058fc82148978562a
sazare <song.of.sand@gmail.com>
2020-08-01 07:09:31 +0900

    add load-ito.lisp
    	new file:   load-ito.lisp

---
+++ load-ito.lisp
@@ -0,0 +1,13 @@
+(myload "ito.lisp")
+(terpri t)
+(terpri t)
+(terpri t)
+(terpri t)
+(terpri t)
+(format t "HERE WE GO~%")
+
+
+(myload "ito.lisp")
+(format t "~%~%... end of loading test utility~%")
+
+
